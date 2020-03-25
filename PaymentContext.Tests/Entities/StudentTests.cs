8c0604b080e0b2f8713af11bb606b934f91cb29c
Nicolas Plaisant <nicolasbcp@gmail.com>
2020-03-25 16:59:29 -0300

    Refatorar Entidades
    Novos métodos implementados e alterações feitas na segurança das mesmas (construtores adicionados, métodos set com visibilidade private, atributos do tipo List passaram a ser IReadOnlyCollection)

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var student  = new Student("Nicolas", "Plaisant", "11111111111", "teste@teste.io");
+        }
+    }
+}
